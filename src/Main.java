37a8264bb544b6f7312273fdb0c4fbfe15474711
Evan <ekjuarez20@gmail.com>
2024-01-27 23:20:44 -0700

    Added warm hello

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Goodbye Loser");
+        System.out.println("Hello again old friend.");
     }
 }
